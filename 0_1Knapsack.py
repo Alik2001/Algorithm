5407e52ac3c2dfc28e6a310eb497970ee70a774d
Musfiqur Rashid <rashid15-5531@diu.edu.bd>
2024-05-08 12:39:11 +0600

    First Commit

---
+++ 0_1Knapsack.py
@@ -0,0 +1,22 @@
+def knapSack(W, wt, val, n, memo):
+    if n == 0 or W == 0:
+        return 0
+
+    if memo[n][W] != -1:  # Check if result for this subproblem is already computed
+        return memo[n][W]
+
+    if wt[n - 1] > W:
+        memo[n][W] = knapSack(W, wt, val, n - 1, memo)
+        return memo[n][W]
+    else:
+        memo[n][W] = max(val[n - 1] + knapSack(W - wt[n - 1], wt, val, n - 1, memo),
+                         knapSack(W, wt, val, n - 1, memo))
+        return memo[n][W]
+
+if __name__ == '__main__':
+    profit = [30, 40, 60]
+    weight = [3, 2, 5]
+    W = 6
+    n = len(profit)
+    memo = [[-1 for _ in range(W + 1)] for _ in range(n + 1)]  # List for memoization
+    print(knapSack(W, weight, profit, n, memo))
